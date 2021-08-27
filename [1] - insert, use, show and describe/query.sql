39c7965643aa54f8ba280d65ffbe3f85f976620a
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-08-26 23:23:56 -0500

    add insert, use, show and describe

---
+++ [1] - insert, use, show and describe/query.sql
@@ -0,0 +1,25 @@
+-- para usar uma determinada base de dados
+use database_name;
+
+-- para mostrar todas as tabelas de uma base de dados
+show tables;
+
+-- para mostrar a descrição das colunas de uma tabela
+describe table_name;
+
+-- para inserir um registro na tabela
+insert into table_name(fields) values(
+  dados
+);
+
+-- para inserir múltiplos registros na tabela
+insert into table_name(fields) values
+(
+  dados
+),
+(
+  dados
+),
+(
+  dados
+);
