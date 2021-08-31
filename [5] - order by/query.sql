781d19c2738a2c9a5a419a36cc1f3dde6dd41588
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-08-30 22:04:17 -0500

    add order by

---
+++ [5] - order by/query.sql
@@ -0,0 +1,6 @@
+-- para selecionar os dados, de forma ordenada, na impressão da tabela
+select * from table_name order by field [asc, desc]; --asc is default
+--asc -> crescente; desc -> decrescente
+
+-- exemplo
+select * from users order by id;
