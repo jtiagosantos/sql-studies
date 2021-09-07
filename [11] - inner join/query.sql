6a11ddc466aea5c88e2ed1c13628791480db4e8c
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-09-06 23:18:14 -0500

    add inner join

---
+++ [11] - inner join/query.sql
@@ -0,0 +1,10 @@
+-- para selecionar múltiplas tabelas ao mesmo tempo(outro método)
+select x.column, y.column from table_1_name as x, table_2_name as y
+inner join table_2_name y
+on x.column = y.column;
+
+-- exemplo
+select g.id as gid, g.description, m.id as mid, m.name as movie_name, m.id_genre
+from genres as g
+inner join movies m
+on g.id = m.id_genre;
