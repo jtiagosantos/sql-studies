eb49fee28a511ec8065a6456ff45c1efc092ebd4
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-09-10 21:00:10 -0500

    update rand e round

--- [14] - rand e round/query.sql
+++ [14] - rand e round/query.sql
@@ -10,3 +10,9 @@ round(rand() * 100, número de casas decimais);
 -- exemplo 
 select round(rand() * 10000, 2);
 -- gera valores aleatórios entre 0 e 1000, com duas casas decimais
+
+-- para filtrar um registro aleatório de uma tabela
+select column or * from table_name order by rand() limit 1;
+
+-- exemplo
+select * from users order by rand() limit 1;
