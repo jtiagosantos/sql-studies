227a9b4cbae1c385f9c878105bd07e8b030d7f7c
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-09-16 04:40:37 -0500

    add update com varios joins

---
+++ [17] - update com vários joins/query.sql
@@ -0,0 +1,8 @@
+-- para atualizar os registros com relacionamento many-to-many
+
+-- exemplo
+update users as u
+join profiles  as p
+on p.user_id = u.id
+set  p.bio = concat(p.bio, "atualizado")
+where u.first_name = "Katelyn";
