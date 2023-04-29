ed46011971d4aed41d6bd1dc2bf164615161e147
Tiago Santos <tiagosan040@gmail.com>
2023-04-29 15:41:09 -0300

    fix: fix content about left join

--- [12] - left join/query.sql
+++ [12] - left join/query.sql
@@ -1,7 +1,7 @@
 -- para trazer os registros da tabela que está mais a esquerda,
 -- independente se tem relacionamento com os registros da tabela à direita
-select x.column, y.column from table_1_name as x, table_2_name as y
-left join table_2_name y
+select x.column, y.column from table_1_name as x
+left join table_2_name as y
 on x.column = y.column;
 
 -- exemplo
